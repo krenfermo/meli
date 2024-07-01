12eb9c8f3ee486f70b0e2395f313746c93317629
krenfermo <morakurt@gmail.com>
2024-07-01 17:46:33 -0600

    inserta en tablas de datos

--- funciones_tablas.py
+++ funciones_tablas.py
@@ -1,48 +1,105 @@
 from init_db import get_db_connection
 import ast
 
+import json
+from psycopg2 import sql
 
 
-def mete_order_items(order_items):
-    
+def insert_orders_items(data, table_name):
+    if not data:
+        return
+ 
     conn=get_db_connection()
     cur = conn.cursor()
+    
+    #for item in data:
+   
+        
 
-    # Iterar sobre los elementos del pedido y guardar cada uno en la base de datos
-    for item in order_items['order_items']:
-        item_data = item['item']
-        quantity = item['quantity']
-        unit_price = item['unit_price']
-        full_unit_price = item['full_unit_price']
-        currency_id = item['currency_id']
-        sale_fee = item['sale_fee']
-        listing_type_id = item['listing_type_id']
-        element_id = item['element_id']
+    # Extraer columnas de los diccionarios
+    try:
+        columns = data[0].keys()
+        columns_str = ', '.join(columns)
+        values_str = ', '.join(['%s'] * len(columns))
 
-        # Ejemplo de consulta SQL para insertar los datos en una tabla llamada 'order_items'
-        sql = """
-        INSERT INTO order_items (item_id, title, category_id, quantity, unit_price, full_unit_price, currency_id, sale_fee, listing_type_id, element_id)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
+        insert_query = sql.SQL(
+            f"INSERT INTO {table_name} ({columns_str}) VALUES ({values_str})"
+        )
+        
+        
+        # Preparar los valores
+        values = [[record[col] for col in columns] for record in data]
+    
+        # Ejecutar la consulta de inserción
+        cur.executemany(insert_query.as_string(conn), values)
+        
+    
+        
+               
+        cur.execute("""  DELETE
+                FROM orders_items s1
+                USING orders_items s2
+                WHERE s1.order_id = s2.order_id AND s1.id < s2.id;""")
+        conn.commit() 
+                
+        #cur.close()
+        #conn.close()
+        
+    except IndexError:
+        #cur.close()
+        #conn.close()
+        return {}
+    print("termina")
+    
+     
 
-        # Ejecutar la consulta con los parámetros correspondientes
-        cur.execute(sql, (
-            item_data['id'],
-            item_data['title'],
-            item_data['category_id'],
-            quantity,
-            unit_price,
-            full_unit_price,
-            currency_id,
-            sale_fee,
-            listing_type_id,
-            element_id
-        ))
 
-    # Confirmar la transacción y cerrar la conexión
-    conn.commit()
-    cur.close()
-    conn.close()
+def insert_orders_datos(data, table_name):
+    if not data:
+        return
+ 
+    conn=get_db_connection()
+    cur = conn.cursor()
+    #for item in data:
+
+    
+
+    # Extraer columnas de los diccionarios
+    try:
+        columns = data[0].keys()
+        columns_str = ', '.join(columns)
+        values_str = ', '.join(['%s'] * len(columns))
+
+        insert_query = sql.SQL(
+            f"INSERT INTO {table_name} ({columns_str}) VALUES ({values_str})"
+        )
+    
+        
+        # Preparar los valores
+        values = [[record[col] for col in columns] for record in data]
+        
+        # Ejecutar la consulta de inserción
+        cur.executemany(insert_query.as_string(conn), values)
+        
+    
+        
+        
+        cur.execute("""  DELETE
+            FROM orders_datos s1
+            USING orders_datos s2
+            WHERE s1.order_id = s2.order_id AND s1.id < s2.id;""")
+        conn.commit() 
+                
+        #cur.close()
+        #conn.close()
+        
+    except IndexError:
+        #cur.close()
+        #conn.close()
+        return {}
+    print("termina")
+        
+        
 
 
 def llena_orders(user_id):
@@ -50,51 +107,93 @@ def llena_orders(user_id):
         conn=get_db_connection()
         cur = conn.cursor()
         
-        cur.execute("SELECT json_data FROM public.orders where user_id='{}' ORDER BY id DESC limit 2".format(user_id))
+        cur.execute("SELECT json_data,user_id FROM public.orders where user_id='{}' ORDER BY id DESC".format(user_id))
         
         orders=cur.fetchall()
  
+        
+        orders_dat=[]
+            
+        for i in range (0,len(orders)):
+
+            json_data=  ast.literal_eval(str(orders[i][0]))  
+
+            payments=[]
+            for item in json_data["payments"]:
+                
+                payments.append( {"total_paid_amount":item["total_paid_amount"], "date_approved": item["date_approved"],
+                                    "order_id":item["order_id"]  } )
+                
+            order_items=[]
+
+            for item in json_data["order_items"]:
+                if item["item"]["seller_sku"] ==None:
+                    seller_sku="N/A"
+                else:
+                    seller_sku=item["item"]["seller_sku"]
+                    
+                
+                order_items.append( {"quantity":item["quantity"], "full_unit_price": item["unit_price"],"unit_price": item["unit_price"],
+                                        "sale_fee":item["sale_fee"],"id":item["item"]["id"],"title":item["item"]["title"],
+                                        "category_id":item["item"]["category_id"],"seller_sku":seller_sku  } )
+                
+
+                
+                orders_dat.append({"payments": payments, "order_items":order_items, "date_created":json_data["date_created"],
+                                "buyer_id": json_data["buyer"]["id"], "buyer_nickname": json_data["buyer"]["nickname"],
+                                "paid_amount":json_data["paid_amount"],"shipping_id":json_data["shipping"]["id"]})
+
+        
+        datos_orders=[]
+        orders_items=[]
+        for item in orders_dat:
+            
+            datos_orders.append({"order_id":item["payments"][0]["order_id"],
+                                "payments_total_paid_amount":item["payments"][0]["total_paid_amount"],
+                                "payments_date_approved":item["payments"][0]["date_approved"],
+                                "date_created":item["date_created"],
+                                "buyer_id":item["buyer_id"],
+                                "buyer_nickname":item["buyer_nickname"],
+                                "paid_amount":item["paid_amount"],
+                                "shipping_id":item["shipping_id"],
+                            "user_id":user_id
+                                })
+        
+            
+            for orders in (item["order_items"]):
+                
+                orders_items.append({"order_id":item["payments"][0]["order_id"],
+                            "quantity":orders["quantity"],
+                            "full_unit_price" :orders["full_unit_price"],
+                            "unit_price" :orders["unit_price"],
+                            "sale_fee": orders["sale_fee"],
+                            "item_id":orders["id"],
+                            "title": orders["title"],
+                            "category_id": orders["category_id"],
+                            "seller_sku": orders["seller_sku"],
+                            "user_id":user_id
+                                })
+        print("va itrems")
+      
+        insert_orders_items(orders_items,"orders_items")
+
  
-        conn.commit()
+        print("va orders_datos")
+        insert_orders_datos(datos_orders,"orders_datos")
+
 
         cur.close()
         conn.close()
 
-        return orders
+        return True
     
     except KeyError:
         pass
         
-        
-        
-        
-orders=[]
+llena=llena_orders("1118811075")
+
 
  
-llena=llena_orders("1411430232")
-
-for i in range (0,len(llena)):
-
-    json_data=  ast.literal_eval(str(llena[i][0]))  
-
-    payments=[]
-    for item in json_data["payments"]:
         
-        payments.append( {"total_paid_amount":item["total_paid_amount"], "date_approved": item["date_approved"],
-                            "order_id":item["order_id"]  } )
-        
-    order_items=[]
-    for item in json_data["order_items"]:
-        
-        order_items.append( {"quantity":item["quantity"], "full_unit_price": item["full_unit_price"],
-                                "sale_fee":item["sale_fee"],"id":item["item"]["id"],"title":item["item"]["title"],
-                                "category_id":item["item"]["category_id"],"seller_sku":item["item"]["seller_sku"]   } )
-    
-    orders.append({"payments": payments, "order_items":order_items, "date_created":json_data["date_created"],
-                    "buyer_id": json_data["buyer"]["id"], "buyer_nickname": json_data["buyer"]["nickname"],
-                    "paid_amount":json_data["paid_amount"],"shipping_id":json_data["shipping"]["id"]})
-    
-print(orders)
-
 
 
--- pruebasIA.py
+++ pruebasIA.py
@@ -1,26 +1,65 @@
-from openai import OpenAI
+#from openai import OpenAI
 
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os
+ 
+
+from langchain.llms import OpenAI
+from langchain.sql_database import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+
 
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 OPENIA_KEY = os.environ.get("OPENIA_KEY")
+print(OPENIA_KEY)
+DB_HOSTNAME = os.environ.get("DB_HOSTNAME")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PORT = os.environ.get("DB_PORT")
+
  
 
-print(OPENIA_KEY)
-client = OpenAI(api_key=OPENIA_KEY)
-
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo", 
-  messages=[
-    {"role": "user", "content": "Can you tell me 5 countries in Latin America?"}
-  ],
-  max_tokens=30
+db = SQLDatabase.from_uri("postgresql+psycopg2://{}:{}@{}:{}/{}".format(DB_USERNAME,DB_PASSWORD,DB_HOSTNAME,DB_PORT,DB_NAME),
 )
 
-print(completion.choices[0].message.content)
+ 
+# setup llm
+llm = OpenAI(temperature=0, openai_api_key=OPENIA_KEY)
 
+# Create db chain
+ 
+
+
+QUERY2 = """
+Given an input question, first create a syntactically correct postgresql query to run, then look at the results of the query and return the answer.
+Use the following format:
+
+Question: Question here
+SQLQuery: SQL Query to run
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
+
+{question}
+"""
+ 
+
+QUERY = """
+Data una pregunta del usuario:
+1. crea una consulta de postgres
+2. revisa los resultados
+3. devuelve el dato
+4. si tienes que hacer alguna aclaración o devolver cualquier texto que sea siempre en español
+#{question}
+"""
+# Setup the database chain
+db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
+
+
+#question = QUERY.format(question="cual es el articulo mas vendido de la tabla orders_items?")
+question = QUERY.format(question="cuales son los productos top 10 mas vendidos de la tabla orders_datos del userid=1118811075?")
+print(db_chain.run(question))
